68061f0ba6df664240298f7b30606b5f24dd0aa9
Sydney Chapman <chapman.sydney44@bloomfield.org>
2018-07-17 19:22:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,22 +1,22 @@
-# require_relative 'config/environment'
+require_relative 'config/environment'
 
-# class App < Sinatra::Base
-#   get '/' do
-#     "Hello World!"
-#   end 
+class App < Sinatra::Base
+  get '/' do
+    "Hello World!"
+  end 
 
-# get '/name' do 
-#   "My name is Sydney"
-# end
+get '/name' do 
+  "My name is Sydney"
+end
 
-# get '/hometown' do 
-#   "My hometown is Detroit"
-# end 
+get '/hometown' do 
+  "My hometown is Detroit"
+end 
 
-# get '/favorite-song' do 
-#   "My favorite song is We belong Together by Mariah Carrey"
-# end 
-# end
+get '/favorite-song' do 
+  "My favorite song is We belong Together by Mariah Carrey"
+end 
+end
 
 get '/hello/:id' do 
   @name = params[:id]
